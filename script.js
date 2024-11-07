cae9ddb9d81191d70e015f480ad799a13c75198e
Benjamin Eren <Ben@sumcoeu.com>
2024-11-07 19:28:36 +0000

    added if logic to stop blank items being added

--- script.js
+++ script.js
@@ -5,35 +5,39 @@ let counter = 0;
 txtinput.addEventListener("keydown", (event) => {
   if (event.key == "Enter") {
     newItem()
-    
   }
 });
 
 function newItem(){
-  counter++
-  // initialise three divs to make 1 for the list item and 2 for in the list
-  const divParent = document.createElement("div");
-  const divChild = document.createElement("div");
-  const divTotal = document.createElement("div");
-  
-  //put these two inside of a div - (divTotal) called "listClass"
-  const newContent = document.createTextNode(txtinput.value);
-  const childContent = document.createTextNode(counter);
+  if (txtinput.value === ""){
+    alert("You didn't enter anything into the text box")
+  } else {
+    counter++
+    // initialise three divs to make 1 for the list item and 2 for in the list
+    const divParent = document.createElement("div");
+    const divChild = document.createElement("div");
+    const divTotal = document.createElement("div");
+    const button2 = document.createElement("button")
+    
+    //put these two inside of a div - (divTotal) called "listClass"
+    const newContent = document.createTextNode(txtinput.value);
+    const childContent = document.createTextNode(counter);
 
-  divChild.appendChild(childContent)
-  divParent.appendChild(newContent)
-  
-  // put the data inside the div to add to the HTML
-  divTotal.appendChild(divParent)
-  divTotal.appendChild(divChild)
+    divChild.appendChild(childContent)
+    divParent.appendChild(newContent)
+    
+    // put the data inside the div to add to the HTML
+    divTotal.appendChild(divParent)
+    divTotal.appendChild(divChild)
 
-  divTotal.id = "div_id" ;
-  divTotal.className = "listClass";
+    divTotal.id = "div_id" ;
+    divTotal.className = "listClass";
 
-  document.getElementById("list").appendChild(divTotal);  
+    document.getElementById("list").appendChild(divTotal);  
 
-  // clear the input box at the end
-  txtinput.value = "";
+    // clear the input box at the end
+    txtinput.value = "";
+  }
 }
 
 function reset(){
@@ -48,4 +52,8 @@ function reset(){
 
     //resets counter 
     counter = 0;
+}
+
+function deleteItem(){
+
 }
